075ae55166bf0886c9ebfd5ad8c2cf617829d3d5
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-11-14 20:38:56 -0300

    circularidade

--- ListasDuplamenteEncadeadas/src/atividades/pontoExtra/LDECircular.java
+++ ListasDuplamenteEncadeadas/src/atividades/pontoExtra/LDECircular.java
@@ -5,8 +5,6 @@ public class LDECircular<T extends Comparable<T>> {
     private LDENode<T> ult;
     private int qtd;
 
-
-
     public boolean isEmpty() {
         if (this.qtd == 0) {
             return true;
@@ -15,143 +13,136 @@ public class LDECircular<T extends Comparable<T>> {
         }
     }
 
-    public void inserirOrdenado (T valor) {
+    public void inserirOrdenado(T valor) {
         LDENode<T> novo = new LDENode(valor);
         LDENode<T> aux, anterior;
         if (this.isEmpty() == true) { // inserir na lista vazia
             this.prim = novo;
             this.ult = novo;
             this.qtd++;
-        }
-        else if (valor.compareTo(this.prim.getInfo()) < 0) { // inserir antes do primeiro
+            this.prim.setAnt(this.ult);
+            this.ult.setProx(this.prim);
+
+        } else if (valor.compareTo(this.prim.getInfo()) < 0) { // inserir antes do primeiro
             novo.setProx(this.prim);
             this.prim.setAnt(novo);
             this.prim = novo;
             this.qtd++;
-        }
-        else if (valor.compareTo(this.prim.getInfo()) == 0) {
+            this.prim.setAnt(this.ult);
+            this.ult.setProx(this.prim);
+        } else if (valor.compareTo(this.prim.getInfo()) == 0) {
             System.out.println("Valor repetido. Inserção não efetuada!");
-        }
-        else if (valor.compareTo(this.ult.getInfo()) > 0) { // inserir depois do último
+        } else if (valor.compareTo(this.ult.getInfo()) > 0) { // inserir depois do último
             this.ult.setProx(novo);
             novo.setAnt(this.ult);
             this.ult = novo;
             this.qtd++;
-        }
-        else if (valor.compareTo(this.ult.getInfo()) == 0) {
+            this.prim.setAnt(this.ult);
+            this.ult.setProx(this.prim);
+        } else if (valor.compareTo(this.ult.getInfo()) == 0) {
             System.out.println("Valor repetido. Inserção não efetuada!");
-        }
-        else { 
-             aux = this.prim.getProx();
-             while (aux != null){
-                 if (valor.compareTo(aux.getInfo()) == 0) {
-                     System.out.println("Valor repetido. Inserção não efetuada!");
-                     break;
-                 }
-                 else if (valor.compareTo(aux.getInfo()) < 0) { // achei local de inserção
-                     anterior = aux.getAnt();
-                     anterior.setProx(novo);
-                     aux.setAnt(novo);
-                     novo.setProx(aux);
-                     novo.setAnt(anterior);
-                     this.qtd++;
-                     break;
-                 }
-                 else {
-                     aux = aux.getProx();
-                 }
-             }       
+        } else {
+            aux = this.prim.getProx();
+            while (aux != null) {
+                if (valor.compareTo(aux.getInfo()) == 0) {
+                    System.out.println("Valor repetido. Inserção não efetuada!");
+                    break;
+                } else if (valor.compareTo(aux.getInfo()) < 0) { // achei local de inserção
+                    anterior = aux.getAnt();
+                    anterior.setProx(novo);
+                    aux.setAnt(novo);
+                    novo.setProx(aux);
+                    novo.setAnt(anterior);
+                    this.qtd++;
+                    this.prim.setAnt(this.ult);
+                    this.ult.setProx(this.prim);
+                    break;
+                } else {
+                    aux = aux.getProx();
+                }
+            }
         }
     }
 
-    public LDENode<T> busca (T valor) { // busca sequencial melhorada
+    public LDENode<T> busca(T valor) { // busca sequencial melhorada
         LDENode<T> aux;
         if (this.isEmpty() == true) {
             return null;
-        }
-        else if (valor.compareTo(this.ult.getInfo()) == 0) {
+        } else if (valor.compareTo(this.ult.getInfo()) == 0) {
             return this.ult;
-        }
-        else if (valor.compareTo(this.ult.getInfo()) > 0) {
+        } else if (valor.compareTo(this.ult.getInfo()) > 0) {
             return null;
-        }
-        else {
+        } else {
             aux = this.prim;
             while (aux.getProx() != null) {
-                if (aux.getInfo().compareTo(valor) == 0) { // achei 
+                if (aux.getInfo().compareTo(valor) == 0) { // achei
                     return aux;
-                }
-                else if (valor.compareTo(aux.getInfo()) > 0) {
+                } else if (valor.compareTo(aux.getInfo()) > 0) {
                     aux = aux.getProx();
-                }
-                else {
+                } else {
                     return null;
                 }
             }
-        }  
+        }
         return null;
     }
 
-    public void remover (T valor) {
+    public void remover(T valor) {
         LDENode<T> retorno = this.busca(valor);
         LDENode<T> anterior, proximo;
-        if (retorno == null){
+        if (retorno == null) {
             System.out.println("Valor não encontrado");
-        }
-        else if (this.qtd == 1){ // lista com apenas um nó
+        } else if (this.qtd == 1) { // lista com apenas um nó
             this.prim = null;
             this.ult = null;
             this.qtd = 0;
-        }
-        else if (retorno == this.prim){ // remover o primeiro
+        } else if (retorno == this.prim) { // remover o primeiro
             this.prim = this.prim.getProx();
             this.prim.setAnt(null);
             this.qtd--;
-        }
-        else if (retorno == this.ult) { // remover o último
+            this.prim.setAnt(this.ult);
+            this.ult.setProx(this.prim);
+        } else if (retorno == this.ult) { // remover o último
             this.ult = this.ult.getAnt();
             this.ult.setProx(null);
             this.qtd--;
+            this.prim.setAnt(this.ult);
+            this.ult.setProx(this.prim);
+        } else { // remover no "meio"
+            anterior = retorno.getAnt();
+            proximo = retorno.getProx();
+            anterior.setProx(proximo);
+            proximo.setAnt(anterior);
+            this.qtd--;
+            this.prim.setAnt(this.ult);
+            this.ult.setProx(this.prim);
         }
-        else { // remover no "meio"
-             anterior = retorno.getAnt();
-             proximo = retorno.getProx();
-             anterior.setProx(proximo);
-             proximo.setAnt(anterior);
-             this.qtd--;
-        }        
     }
 
-    public void exibirInfo(T valor){
+    public void exibirInfo(T valor) {
         LDENode<T> resultadoBusca = this.busca(valor);
 
-        if(resultadoBusca != null){
+        if (resultadoBusca != null) {
             System.out.println(resultadoBusca.getInfo());
-        }
-        else{
+        } else {
             System.out.println("Matrícula não encontrada");
         }
 
     }
 
-    public void exibirInfoTodos(){
+    public void exibirInfoTodos() {
         LDENode<T> aux = this.prim;
-        if(this.isEmpty()){
+        if (this.isEmpty()) {
             System.out.println("lista vazia@");
-        }
-        else if(this.qtd == 1){
+        } else if (this.qtd == 1) {
             System.out.println(aux.getInfo());
-        }
-        else{
-            while(aux != null){
+        } else {
+            for (int i = 0; i < this.qtd; i++) {
                 System.out.println(aux.getInfo());
                 aux = aux.getProx();
             }
         }
-        
+
     }
-
-    
-
 
 }
