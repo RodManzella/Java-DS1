cc01f936614df772c76ee48c42f6281f4fb103aa
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-11-14 19:35:57 -0300

    altos commits

--- ListasDuplamenteEncadeadas/src/atividades/pontoExtra/App.java
+++ ListasDuplamenteEncadeadas/src/atividades/pontoExtra/App.java
@@ -10,49 +10,62 @@ public class App {
         Cadastro turma2 = new Cadastro();
         Cadastro turma3 = new Cadastro();
         Cadastro turma4 = new Cadastro();
+        Cadastro turmaEscolhida = null;
 
-        do{
+        do {
             exibirOpcoes();
-            escolhaTurma = scan.nextInt(); scan.nextLine();
+            escolhaTurma = scan.nextInt();
+            scan.nextLine();
 
-            if(escolhaTurma == 1){
-                ITurma();
-                escolhaOpcao = scan.nextInt(); scan.nextLine();
-                if(escolhaOpcao == 1){
-                    System.out.println("Digite o nome do aluno: ");
-                    String nome = scan.nextLine();
-                    System.out.println("Digite a matrícula do aluno: ");
-                    String matricula = scan.nextLine();
+            if (escolhaTurma == 1) {
+                turmaEscolhida = turma1;
+            } else if (escolhaTurma == 2) {
+                turmaEscolhida = turma2;
+            } else if (escolhaTurma == 3) {
+                turmaEscolhida = turma3;
+            } else if (escolhaTurma == 4) {
+                turmaEscolhida = turma4;
+            }
+            ITurma();
+            escolhaOpcao = scan.nextInt(); scan.nextLine();
 
-                    Aluno alunoInserir = new Aluno(matricula, nome);
-                    turma1.cadastrar(alunoInserir); 
-                }
-                else if(escolhaOpcao == 2){
-                    turma1.listar();
-                }
-                else if(escolhaOpcao == 3){
-                    System.out.println("Digite a matrícula do aluno: ");
-                    String matricula = scan.nextLine();
+            if (escolhaOpcao == 1) {
+                System.out.println("Digite o nome do aluno: ");
+                String nome = scan.nextLine();
+                System.out.println("Digite a matrícula do aluno: ");
+                String matricula = scan.nextLine();
 
-                    turma1.alterarMediaFinal(matricula);
-                }
-                else if(escolhaOpcao == 4){
-                    System.out.println("Digite o número de faltas do aluno: ");
-                    String matricula = scan.nextLine();
+                Aluno alunoInserir = new Aluno(matricula, nome);
+                turmaEscolhida.cadastrar(alunoInserir);
+            } else if (escolhaOpcao == 2) {
+                turmaEscolhida.listar();
+            } else if (escolhaOpcao == 3) {
+                System.out.println("Digite a matrícula do aluno: ");
+                String matricula = scan.nextLine();
 
-                    turma1.alterarFaltas(matricula);
-                }
-                
+                turmaEscolhida.alterarMediaFinal(matricula);
+            } else if (escolhaOpcao == 4) {
+                System.out.println("Digite o número de faltas do aluno: ");
+                String matricula = scan.nextLine();
+
+                turmaEscolhida.alterarFaltas(matricula);
+            } else if (escolhaOpcao == 5) {
+                System.out.println("Digite a matrícula do aluno: ");
+                String matricula = scan.nextLine();
+
+                turmaEscolhida.exibir(matricula);
+            } else if (escolhaOpcao == 6) {
+                System.out.println("Digite a matrícula do aluno: ");
+                String matricula = scan.nextLine();
+
+                turmaEscolhida.removerAluno(matricula);
             }
 
-        }while(escolhaTurma != 0);
-
-        
-
+        } while (escolhaTurma != 0);
 
     }
 
-    public static void exibirOpcoes(){
+    public static void exibirOpcoes() {
         System.out.println("1 -  Operações da turma 1");
         System.out.println("2 -  Operações da turma 2");
         System.out.println("3 -  Operações da turma 3");
@@ -67,4 +80,5 @@ public class App {
         System.out.println("5 - Exibir dados de aluno");
         System.out.println("6 - Remover aluno cadastrado");
     }
+
 }
