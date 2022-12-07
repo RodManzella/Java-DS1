af5e73924a7b6496021a33b6eb92fa1ada4226e5
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-12-07 15:59:20 -0300

    binary

--- binaryTree/src/BinaryTree.java
+++ binaryTree/src/BinaryTree.java
@@ -1,4 +1,60 @@
 public class BinaryTree <T extends Comparable<T>> {
     private Node raiz;
+
+
+    public boolean isEmpty() {
+        if (this.raiz == null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void insert(T value) {
+        if (this.isEmpty() == true) {
+            this.raiz = new Node<>(value);
+        } else {
+            this.raiz.insertNode(value);
+        }
+    }
+
+
+    public T buscar(T valor) {
+        Node<T> aux;
+        if (this.isEmpty() == true) {
+            return null;
+        } else {
+            aux = this.raiz;
+            while (aux != null) {
+                if (valor.compareTo(aux.getInfo()) == 0) {
+                    return aux.getInfo(); // Achou!!!
+                } else if (valor.compareTo(aux.getInfo()) < 0) {
+                    aux = aux.getLeft();
+                } else {
+                    aux = aux.getRight();
+                }
+            }
+            return null; // Não achou!!!            
+        }
+    }
+
     
+
+    public void emOrdem() {
+        if (this.isEmpty() == true) {
+            System.out.println("Árvore vazia");
+        } else {
+            this.percorrerEmOrdem(this.raiz);
+        }
+    }
+
+    private void percorrerEmOrdem(Node<T> r) {
+        if (r != null) {
+            percorrerEmOrdem(r.getLeft());
+            System.out.println(r.getInfo());
+            percorrerEmOrdem(r.getRight());
+        }
+    }
+
+
 }
